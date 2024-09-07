5f63df124bca0bc834593aac169c5d52f9670f64
Avinash Pandey <Avinash.ranjan21@gmail.com>
2024-09-07 21:20:07 +0530

    chore: Update compileSdk and targetSdk to 35
    Updates the compileSdk and targetSdk versions to 35 to target the latest Android API
     level.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.ecommercesample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.ecommercesample"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
